a1512ba347a64c87286cc42f98f64d48d577a90b
Altusha4 <240782@astanait.edu.kz>
2025-09-25 13:24:21 +0600

    Added Adaptee

--- src/emojiadapter/EmojiNotificationService.java
+++ src/emojiadapter/EmojiNotificationService.java
@@ -1,4 +1,7 @@
 package emojiadapter;
 
 public class EmojiNotificationService {
+    public void deliver(String emojiMessage) {
+        System.out.println("[EMOJI SERVICE] " + emojiMessage);
+    }
 }
